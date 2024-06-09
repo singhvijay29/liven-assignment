f982ba488f96465d3f7aea8aac16ed3adaad0ccc
singhvijay29 <singhvijay8529@gmail.com>
2024-06-09 16:52:28 +0530

    Added purchase icon

--- src/component/CommonPurchaseItem.jsx
+++ src/component/CommonPurchaseItem.jsx
@@ -7,8 +7,8 @@ const CommonPurchaseItem = ({ product }) => {
       className="shadow-lg border-[2px] border-gray-300 rounded-[16px] px-8 py-4 bg-white flex items-center justify-between relative cursor-pointer"
     >
       {product?.exclusive && (
-        <div className="absolute bg-[#FF3B6E] rounded-[16px] flex items-center gap-2 px-4 py-1 top-[-16px] left-1/2 -translate-x-1/2">
-          <img src="/logo.svg" className="h-[16px]" />
+        <div className="absolute bg-[#FF3B6E] rounded-[16px] flex items-center gap-2 px-4 py-1 top-[-16px] left-1/2 -translate-x-1/2 w-[172px]">
+          <img src="/logo.svg" className="h-[16px]" alt="logo" />
           <div className="font-semibold text-white">App Exclusive</div>
         </div>
       )}
@@ -18,7 +18,11 @@ const CommonPurchaseItem = ({ product }) => {
           ${product?.buy}
         </div>
       </div>
-      <div></div>
+      <img
+        src="/purchase.png"
+        className="w-[66px] absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+        alt="purchase"
+      />
       <div className="text-right">
         <div className="text-[16px] font-bold leading-[16px] text-[#56b5a2]">
           Bonus
--- src/component/HomeScreen.jsx
+++ src/component/HomeScreen.jsx
@@ -54,29 +54,31 @@ const HomeScreen = () => {
   return (
     <div className="max-w-[506px] bg-[#F9F9F9] h-[100vh] mx-auto relative">
       <Navbar />
-      <div className="max-w-[304px] mx-auto flex flex-col gap-2 p-2 overflow-y-scroll">
-        <div className="text-[32px] font-bold text-right">San Dollars</div>
-        <div className="font-semibold text-gray-500 text-center">
-          Pay now,eat more later! Earn a bonus when you buy food upfront. Tap
-          now to purchase.
+      <div className="h-[calc(100vh-70px)] overflow-y-scroll no-scrollbar">
+        <div className="max-w-[304px] mx-auto flex flex-col gap-2 p-2">
+          <div className="text-[32px] font-bold text-right">San Dollars</div>
+          <div className="font-semibold text-gray-500 text-center">
+            Pay now,eat more later! Earn a bonus when you buy food upfront. Tap
+            now to purchase.
+          </div>
         </div>
+        <div className="pl-4 pr-1 py-1 rounded-[24px] bg-[#F2F2F2] w-max flex gap-2 items-center mx-auto mt-3">
+          <GiftIcon />
+          <div className="font-semibold text-[#726565]">Purchase as gift</div>
+          <TogglePill
+            isGiftedProduct={isGiftedProduct}
+            setIsGiftedProduct={setIsGiftedProduct}
+          />
+        </div>
+        <div className="px-6 mt-9 flex flex-col gap-3  overflow">
+          {(isGiftedProduct ? gift_products : non_gift_products)?.map(
+            (product, index) => (
+              <CommonPurchaseItem product={product} key={index} />
+            )
+          )}
+        </div>
+        <Footer />
       </div>
-      <div className="pl-4 pr-1 py-1 rounded-[24px] bg-[#F2F2F2] w-max flex gap-2 items-center mx-auto mt-3">
-        <GiftIcon />
-        <div className="font-semibold text-[#726565]">Purchase as gift</div>
-        <TogglePill
-          isGiftedProduct={isGiftedProduct}
-          setIsGiftedProduct={setIsGiftedProduct}
-        />
-      </div>
-      <div className="px-6 mt-9 flex flex-col gap-3">
-        {(isGiftedProduct ? gift_products : non_gift_products)?.map(
-          (product, index) => (
-            <CommonPurchaseItem product={product} key={index} />
-          )
-        )}
-      </div>
-      <Footer />
     </div>
   );
 };
